3e99dbc5bb141b226924d8f646f860cef4a7d777
ShakyaEmpire <64647503+ShakyaEmpire@users.noreply.github.com>
2025-04-22 12:04:21 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -11,7 +11,7 @@ $email_body = "User Name: $name\n".
 "User Email: $visitor_email\n".
 "User Message: $message\n";
 
-$to = 'shakya.empire@gmail.com';
+$to = 'sachinshakya69@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers = "Reply-To : $visitor_email \r\n";
 
